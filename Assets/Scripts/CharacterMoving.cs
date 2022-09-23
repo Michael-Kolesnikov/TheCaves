44ed91723b4aa91dae6fbe398ecc6845259fabc5
L-glance <dark0pesto7@mail.ru>
2022-09-23 21:14:42 +0300

    remake character moving

--- Assets/Scripts/CharacterMoving.cs
+++ Assets/Scripts/CharacterMoving.cs
@@ -3,50 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class CharacterMoving : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public CharacterController characterController;
-
-
-    private float gravity = -19.62f;
-    private Vector3 velocity;
-    private float jumpHeight = 1f;
     GroundCheck check;
+    private Vector3 velocity;
+    private float runSpeed;
+    public float moveSpeed = 5f;
+    private float gravity = -19.62f;
+    private float jumpHeight = 1f;
 
-    private float runSpeed = 9f;
     public KeyCode runningKey = KeyCode.LeftShift;
+
     private void Start()
     {
-        check = GetComponentInChildren<GroundCheck>();      
+        check = GetComponentInChildren<GroundCheck>();
+        runSpeed = moveSpeed * 1.15f;
     }
     void Update()
     {
         float characterSpeed =  moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
 
         Vector3 move = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-        if (Input.GetKey(runningKey) && check.isGrounded)
+        if (Input.GetKey(runningKey))
         {
             characterSpeed = runSpeed;
         }
+        move = Vector3.ClampMagnitude(move, moveSpeed);
         characterController.Move(move * characterSpeed * Time.deltaTime);
 
-        velocity.y += gravity * Time.deltaTime ;
 
 
         if (Input.GetButtonDown("Jump") && check.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-
         if (check.isGrounded && velocity.y < 0)
         {
             velocity.y = 0f;
         }
+
         characterController.Move(velocity * Time.deltaTime);
-
-        
-
-
     }
 }
