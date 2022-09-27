1ded95f0c1d087bc0e62f2b9bb52e7de051a3938
L-glance <dark0pesto7@mail.ru>
2022-09-27 13:44:48 +0300

    destoy "picked up" items by using Ray

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class InventoryController : MonoBehaviour
 {
     private bool isOpened = false;
     public GameObject UIPanel;
     public List<InventorySlot> itemList = new List<InventorySlot>();
+    Camera mainCamera;
+    float reachDistance = 3f;
+
+
+    public GameObject spawn;
     void Start()
     {
+        mainCamera = Camera.main;
         UIPanel.SetActive(false);
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        Cursor.visible = true;
+        if (Input.GetKeyDown(KeyCode.I))
         {
             isOpened = !isOpened;
             bool state = isOpened;
@@ -22,17 +30,21 @@ public class InventoryController : MonoBehaviour
             CharacterMoving.isMove = !state;
             CameraController.isMove = !state;
         }
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if(Input.GetKeyDown(KeyCode.E))
+
+        // уничтожение подобранных предметов
+        RaycastHit hit;
+        if (Input.GetKey(KeyCode.E) )
         {
-            Ray hit;
-            Physics.Raycast(hit);
+            Physics.Raycast(ray, out hit, reachDistance);
+            if (hit.collider?.gameObject.GetComponent<Item>() != null)
+                Destroy(hit.collider.gameObject);
         }
 
     }
     private void CursorChangeState(bool state)
     {
         Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
-        Cursor.visible = state;
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public ScriptableObject itemScriptableObject;
+    public int Amount;
+}
